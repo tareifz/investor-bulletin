007b553e91b507ec07df1213b2879ca512516324
Ahmed Alsaif <32619405+Ahmed-NS@users.noreply.github.com>
2023-12-15 12:25:28 +0000

    fix: market_controllers import + db container name

--- dev_setup/up.sh
+++ dev_setup/up.sh
@@ -7,4 +7,4 @@ docker-compose -f $SCRIPTPATH/docker-compose.yml up -d
 sleep 3
 
 # SETUP DATABASE ROLES AND PERMISSIONS
-docker exec database sh -c "/cockroach/cockroach sql -u root --insecure  --host=database-node < /docker-entrypoint-initdb.d/init.sql"
+docker exec database-node sh -c "/cockroach/cockroach sql -u root --insecure  --host=database-node < /docker-entrypoint-initdb.d/init.sql"
--- phase_3/investor_bulletin/api/routes.py
+++ phase_3/investor_bulletin/api/routes.py
@@ -1,4 +1,4 @@
-# from api.controllers.market.controllers import router as MarketRouter
+# from api.controllers.market_controllers import router as MarketRouter
 
 def init_routes(app):
     # app.include_router(MarketRouter, prefix="/market", tags=["Market"])
